23411053c7ff1ab8000db9315c64fb2c7257fcfb
scholarzainab123 <zainabakinola2010@gmail.com>
2023-09-12 08:48:23 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,25 +1,16 @@
 #include "main.h"
 
-/**
- * main - main function
- *
- * Description: print all the characters contained in _putchar
- * then followed by a new line. _putchar function declared in
- * the main.h file will be used to print the characters.
- *
- * Return: always 0
- */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-
-	return (0);
+    put_char('_');
+    put_char('p');
+    put_char('u');
+    put_char('t');
+    put_char('c');
+    put_char('h');
+    put_char('a');
+    put_char('r');
+    put_char('\n');
+    
+    return (0);
 }
