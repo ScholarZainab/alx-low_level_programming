147867160ba8e24a0a18a56bc3f6b85723c314d8
ScholarZainab <zainabakinola2010@gmail.com>
2023-09-21 17:41:10 -0700

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -8,4 +8,6 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
 char *rot13(char *);
-void print_number(int n);
+void print_number(int n)
+char *infinite_add(char *n, char *m, char *r, int size_r);
+
